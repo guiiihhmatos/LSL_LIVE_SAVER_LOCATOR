8ce92a593bc4391f56bbffc3e654e7a4fbc61b4d
guiiihhmatos <gui.santana.matos@gmail.com>
2023-09-22 09:41:32 -0300

    REFACTOR - JOIN COLUMN

--- backend/src/main/java/com/lts/backend/models/Motorista.java
+++ backend/src/main/java/com/lts/backend/models/Motorista.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -23,7 +24,7 @@ public class Motorista extends Usuario{
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
-	@Column(nullable = true)
+	@JoinColumn(nullable = true)
 	@ManyToMany(mappedBy = "motoristas")
     private Set<Ambulancia> ambulancias = new HashSet<>();
 
