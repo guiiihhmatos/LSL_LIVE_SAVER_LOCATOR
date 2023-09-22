ebe425e14adf312bd7650cf6034ece75d74d0c0b
caiorodriveira <rodrigocaio2775@gmail.com>
2023-09-22 17:15:00 -0300

    IMPROVEMENT - menu criado

---
+++ frontend-hospital/src/app/components/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+
+  // ngOnInit(): void {
+  //   let w;
+  //   for(let i = 10; i <= 100; i+=10){
+  //     w = w + `.font-${i}{font-size: ${i}px}`;
+  //   }
+  //   console.log(w)
+  // }
+
+}
