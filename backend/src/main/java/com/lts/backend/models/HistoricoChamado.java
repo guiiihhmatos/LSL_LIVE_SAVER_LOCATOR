27e91e46e1b523edac39ee1000c176bf1d71f15e
Caio Rodrigo <rodrigocaio2775@gmail.com>
2023-11-09 09:11:14 -0300

    REFACTOR - ajusta identity das models de usuario e historico

--- backend/src/main/java/com/lts/backend/models/HistoricoChamado.java
+++ backend/src/main/java/com/lts/backend/models/HistoricoChamado.java
@@ -17,7 +17,7 @@ import lombok.Data;
 public class HistoricoChamado {
 	
 	 @Id
-	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
 
 	 @Column(name = "dt_chamado")
--- backend/src/main/java/com/lts/backend/models/UsuarioHospital.java
+++ backend/src/main/java/com/lts/backend/models/UsuarioHospital.java
@@ -25,7 +25,7 @@ public class UsuarioHospital extends Usuario{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String login;
