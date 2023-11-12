dce35cacf8e5cc0b5f1c45bc52d73a2f7081b3cf
Guilherme Santana <gui.santana.matos@gmail.com>
2023-11-12 14:13:56 -0300

    IMPROVEMENT - CHAMADO PARA O MOTORISTA

--- frontend-motorista/src/app/pages/home/home/home.component.ts
+++ frontend-motorista/src/app/pages/home/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Chamado } from 'src/app/models/chamado/chamado.model';
+import { Chamado, TiposEmergencia } from 'src/app/models/chamado/chamado.model';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ChamadoService } from 'src/app/services/chamado/chamado.service';
 import { Motorista } from '../../../../../../frontend-hospital/src/app/models/motorista/motorista.model';
@@ -14,6 +14,7 @@ export class HomeComponent {
   chamados: Chamado[] = [];
   idMotorista : number
   motorista : Motorista
+  class = 'tipo'
 
   constructor
   (
@@ -28,12 +29,39 @@ export class HomeComponent {
   ngOnInit()
   {
     this.getAllChamados(this.idMotorista)
+
+
   }
 
   getAllChamados(id : number) {
+
+    let div = document.getElementById('tipo') as HTMLInputElement
+    let icone = document.getElementById('icone') as HTMLInputElement
+
       this.chamadoService.getChamadoByMotorista(id).subscribe({
       next: (res: any) => {
         this.chamados = res;
+        console.log(this.chamados)
+        console.log(this.motorista)
+
+        if(this.chamados[0].tipoEmergencia == TiposEmergencia.GRAVE)
+        {
+          this.class += 'tipo font-tipo-red font-red'
+
+          console.log(this.chamados[0].tipoEmergencia)
+        }
+        else if(this.chamados[0].tipoEmergencia == TiposEmergencia.MUITO_URGENTE)
+        {
+          this.class += 'tipo font-tipo-orange font-orange'
+
+          console.log(this.chamados[0].tipoEmergencia)
+        }
+        else
+        {
+          this.class += 'tipo font-tipo-yellow font-yellow'
+
+          console.log(this.chamados[0].tipoEmergencia)
+        }
       }
     });
   }
